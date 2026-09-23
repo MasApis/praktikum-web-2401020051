b6a5b45b0d440be74c3c4ec936e9d1a013fdaca6
abdul hafidz <abdulhafidz3105@gmail.com>
2026-09-23 18:59:08 +0700

    Pertemuan 2 latihan PHP dasar

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/latihan-php', function () {
+    $nama = 'Abdul Hafidz';
+    $nilai = [45, 65, 60, 35, 10];
+
+    $hitungRataRata = function (array $data): float {
+        $total = 0;
+        foreach ($data as $angka) {
+            $total += $angka;
+        }
+        return $total / count($data);
+    };
+
+    $rataRata = $hitungRataRata($nilai);
+    if ($rataRata >= 75) {
+        $status = 'Lulus';
+    } else {
+        $status = 'Perlu Perbaikan';
+    }
+
+    return view('latihan-php', compact(
+        'nama', 'nilai', 'rataRata', 'status'
+    ));
 });
